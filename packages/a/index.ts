7306e35ee3d954acc6885abf81387ae2a41744e3
fuyunlong <1273179133@qq.com>
2023-01-26 22:20:19 +0800

    feat(*): 初始化package版本

--- packages/a/index.ts
+++ packages/a/index.ts
@@ -1,5 +1,3 @@
-console.log('This is a package v0.0.1')
-console.log('Hello World!')
+console.log('This is a pkg-a')
 
 export const isEven = (x: number) => x % 2 === 0
-console.log(3)
--- packages/b/index.ts
+++ packages/b/index.ts
@@ -1,3 +1,4 @@
 import { isEven } from 'sunrise-pkg-a'
+console.log('this is pkg-b')
 
 export const isOdd = (x: number) => !isEven(x)
